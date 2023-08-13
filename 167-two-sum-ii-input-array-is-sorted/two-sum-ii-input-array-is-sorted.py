3e13076023134a4cd3e55457dd36363bb1c181ed
Aritra Banerjee <98468370+Aritra2019@users.noreply.github.com>
2023-08-13 13:45:18 +0530

    Time: 87 ms (93.65%) | Memory: 14 MB (98.78%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -5,12 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        countS={}
-        myList=[]
-        for i in range (0,len(numbers)):
-            l=target-numbers[i]
-            if(l in countS):
-                myList.append(countS[l]+1)
-                myList.append(i+1)
-            countS[numbers[i]]=i
+        myList = []
+        i = 0
+        j = len(numbers) - 1  # Start from the last index
+        while i < j:  # Use a loop condition to prevent going beyond the middle
+            if numbers[i] + numbers[j] > target:
+                j -= 1
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+            else:
+                myList.append(i + 1)
+                myList.append(j + 1)
+                break  # Stop the loop when a solution is found
         return myList
