d6ca625c62c3f028602224f8cbd222f610377949
Aritra Banerjee <98468370+Aritra2019@users.noreply.github.com>
2024-07-05 09:26:32 +0530

    Time: 10 ms (56.14%) | Memory: 12.8 MB (41.04%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char,int>mpp;
+        mpp['I']=1;
+        mpp['V']=5;
+        mpp['X']=10;
+        mpp['L']=50;
+        mpp['C']=100;
+        mpp['D']=500;
+        mpp['M']=1000;
+        int n=s.size();
+        int no=0;
+        int prev=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            if(mpp[s[i]]<prev){
+                no=no-mpp[s[i]];
+                prev=mpp[s[i]];
+            }
+            else{
+                no=no+mpp[s[i]];
+                prev=mpp[s[i]];
+            }
+        }
+        return no;
+    }
+};
