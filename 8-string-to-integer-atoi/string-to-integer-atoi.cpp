48de21c53af4afd6a8215cdbb7c9f556a26e803f
Aritra Banerjee <98468370+Aritra2019@users.noreply.github.com>
2024-07-07 10:47:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (90.29%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -7,40 +7,32 @@ public:
         int ascii;
         int ascii2;
         int ans = 0;
-
-        // Skip leading whitespaces
         if (s[0] == ' ') {
             for (int i = 0; i < n; i++) {
                 if (s[i] == ' ') {
                     cnt++;
                 } else {
-                    break; // stop at the first non-space character
+                    break; 
                 }
             }
         }
-
-        // Handle optional sign
         if (s[cnt] == '-') {
             neg = true;
             cnt++;
         } else if (s[cnt] == '+') {
             cnt++;
         }
-
-        // Convert characters to integer
         for (int i = cnt; i < n; i++) {
             ascii = (int)s[i];
 
             if (i + 1 < n) {
                 ascii2 = (int)s[i + 1];
             } else {
-                ascii2 = -1; // Ensure ascii2 is not valid if out of bounds
+                ascii2 = -1; 
             }
 
             if (ascii >= 48 && ascii <= 57) {
                 int digit = ascii - 48;
-
-                // Check for overflow/underflow
                 if (ans > (INT_MAX - digit) / 10) {
                     return neg ? INT_MIN : INT_MAX;
                 }
@@ -50,8 +42,6 @@ public:
                 break;
             }
         }
-
-        // Apply sign
         if (neg) {
             ans = -ans;
         }
