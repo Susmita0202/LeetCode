382d1ad914045031396b60f11c077a1a45016d65
Aritra Banerjee <98468370+Aritra2019@users.noreply.github.com>
2024-07-21 12:24:54 +0530

    Time: 8 ms (7.43%) | Memory: 14.8 MB (5.40%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -16,29 +16,17 @@ public:
         
         ListNode* first = dummy;
         ListNode* second = dummy;
-        
-        // Move the first pointer n+1 steps ahead
         for (int i = 0; i <= n; ++i) {
             first = first->next;
         }
-        
-        // Move both pointers until first reaches the end
         while (first != nullptr) {
             first = first->next;
             second = second->next;
         }
-        
-        // second->next is the node to be deleted
         ListNode* nodeToDelete = second->next;
         second->next = second->next->next;
-        
-        // Clean up the node to be deleted
         delete nodeToDelete;
-        
-        // Get the new head
         ListNode* newHead = dummy->next;
-        
-        // Clean up the dummy node
         delete dummy;
         
         return newHead;
