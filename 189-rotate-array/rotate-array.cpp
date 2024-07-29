ebfcd0e4704781070f0bb5f0f805552fd35b0532
Aritra Banerjee <98468370+Aritra2019@users.noreply.github.com>
2024-07-29 20:27:24 +0530

    Time: 27 ms (10.68%) | Memory: 27.9 MB (33.27%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size(); // Ensure k is within the range of nums.size()
+        k = k % nums.size(); 
         if (k == 0) return; 
         int temp[k];
-        // Copy the last k elements to temp
         for (int i = nums.size() - k, j = 0; i < nums.size(); ++i, ++j) {
             temp[j] = nums[i];
         }
-        // Shift elements to the right by k positions
         for (int i = nums.size() - 1; i >= k; --i) {
             nums[i] = nums[i - k];
         }
-        // Copy elements from temp back to nums
         for (int i = 0; i < k; ++i) {
             nums[i] = temp[i];
         }
