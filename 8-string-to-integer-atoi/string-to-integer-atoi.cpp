abb0b3be7df3574d164487879bbf8ff31dd87ed7
Aritra Banerjee <98468370+Aritra2019@users.noreply.github.com>
2024-08-13 09:54:38 +0530

    Time: 3 ms (58.18%) | Memory: 8.9 MB (53.73%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,51 +1,38 @@
 class Solution {
 public:
     int myAtoi(string s) {
-         int n = s.size();
+        int n = s.size();
         int cnt = 0;
         bool neg = false;
         int ascii;
-        int ascii2;
         int ans = 0;
-        if (s[0] == ' ') {
-            for (int i = 0; i < n; i++) {
-                if (s[i] == ' ') {
-                    cnt++;
-                } else {
-                    break; 
-                }
-            }
+        
+        // Skip leading spaces
+        while (cnt < n && s[cnt] == ' ') {
+            cnt++;
         }
-        if (s[cnt] == '-') {
+        
+        // Check for optional sign
+        if (cnt < n && s[cnt] == '-') {
             neg = true;
             cnt++;
-        } else if (s[cnt] == '+') {
+        } else if (cnt < n && s[cnt] == '+') {
             cnt++;
         }
-        for (int i = cnt; i < n; i++) {
-            ascii = (int)s[i];
-
-            if (i + 1 < n) {
-                ascii2 = (int)s[i + 1];
-            } else {
-                ascii2 = -1; 
+        
+        // Convert digits to integer
+        while (cnt < n && s[cnt] >= '0' && s[cnt] <= '9') {
+            int digit = s[cnt] - '0';
+            
+            // Handle overflow/underflow
+            if (ans > (INT_MAX - digit) / 10) {
+                return neg ? INT_MIN : INT_MAX;
             }
-
-            if (ascii >= 48 && ascii <= 57) {
-                int digit = ascii - 48;
-                if (ans > (INT_MAX - digit) / 10) {
-                    return neg ? INT_MIN : INT_MAX;
-                }
-
-                ans = ans * 10 + digit;
-            } else {
-                break;
-            }
+            
+            ans = ans * 10 + digit;
+            cnt++;
         }
-        if (neg) {
-            ans = -ans;
-        }
-
-        return ans;
+        
+        return neg ? -ans : ans;
     }
 };
