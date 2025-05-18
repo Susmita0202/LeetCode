47bf774c32d4b52d641ccbddeb8ec726b8616ae3
Susmita Gorai <132455480+Susmita0202@users.noreply.github.com>
2025-05-18 09:06:16 +0530

    Create FindAPeakElementII.java
    Time: O(mlogn)
    Binary search over (n) columns, each requiring O(m) time to find the column max.
    
    Space: O(1)
    No extra space used.

---
+++ 2047-find-a-peak-element-ii/FindAPeakElementII.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] findPeakGrid(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int low = 0, high = n - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int maxRow = 0;
+            for (int i = 0; i < m; i++) {
+                if (mat[i][mid] > mat[maxRow][mid]) {
+                    maxRow = i;
+                }
+            }
+            
+            boolean leftIsBigger = mid > 0 && mat[maxRow][mid - 1] > mat[maxRow][mid];
+            boolean rightIsBigger = mid < n - 1 && mat[maxRow][mid + 1] > mat[maxRow][mid];
+            
+            if (!leftIsBigger && !rightIsBigger) {
+                return new int[]{maxRow, mid};
+            } else if (leftIsBigger) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
