ebfc6b82390bb5c369ee3ea679fe24cd182ae0ec
Susmita Gorai <132455480+Susmita0202@users.noreply.github.com>
2025-09-07 07:51:40 +0530

    Create IntersectionOfTwoLinkedLists.c
    Time Complexity- O(mn) 
    Space Complexity- O(1)

---
+++ 160-intersection-of-two-linked-lists/IntersectionOfTwoLinkedLists.c
@@ -0,0 +1,14 @@
+public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        while(headA!=null)
+        {
+            ListNode temp = headB;
+            while(temp!=null)
+            {
+                if(temp==headA)
+                    return headA;
+                temp = temp.next;
+            }
+            headA = headA.next;
+        }
+        return null;
+    }
